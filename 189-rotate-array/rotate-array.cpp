ce494a5a85f580f426a85eaa0f057bef32be0d04
Harshal Rajesh Jagtap <harshaljagtap314@gmail.com>
2026-08-18 23:03:19 +0530

    Time: 3 ms (13.38%) | Memory: 263.4 MB (6.03%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+
+void rotate(vector<int> &nums,int k){
+    int n=nums.size();
+    k=k%n;
+    if(n == 0 || k == 0){
+        return ; 
+    }
+    reverse(nums.begin(),nums.begin() + (n-k));
+    reverse(nums.begin() + (n-k),nums.end());
+    reverse(nums.begin() ,  nums.end());
+}
+};
