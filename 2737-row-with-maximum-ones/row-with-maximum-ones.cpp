595ca0a7d1c7f1cf9f227f8b3677017eef03b2e3
Harshal Rajesh Jagtap <harshaljagtap314@gmail.com>
2026-09-13 12:42:14 +0530

    Time: 0 ms (100.00%) | Memory: 64.7 MB (38.62%) - LeetSync

---
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int maxi = 0;
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+
+            for (int j = 0; j < m; j++) {
+                cnt += mat[i][j];
+            }
+
+            if (cnt > maxi) {
+                maxi = cnt;
+                index = i;
+            }
+        }
+
+        return {index, maxi};
+    }
+};
